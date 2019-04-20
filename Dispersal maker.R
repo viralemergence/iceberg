a5fb8633b9f06ff2c09d072d81c082f8d03ba0d1
CJ Carlson <cjcarlson@berkeley.edu>
2019-04-20 10:38:47 -0400

    blank raster fixed

--- Dispersal maker.R
+++ Dispersal maker.R
@@ -1,28 +1,23 @@
 library(ncdf4)
 library(raster)
 
-# Blank raster
 
-setwd("D:/ICEBERG")
-ncin <- nc_open("states.nc")
+### fix blank
 
-get <- function(varname) {
-  soilm_array <- ncvar_get(ncin,varname,start=c(1,1,1166))
-  slice <- raster(t(soilm_array))
-  raster::extent(slice) <- c(-180,180,-90,90)
-  return(slice)
-}
-lutypes <- names(ncin$var)[1]
-blank <- lapply(lutypes,get)[[1]]
-
-
+blank <- matrix(0,720,360)
+blank <- blanker(blank)
+extent(blank) <- c(-180,180,-90,90)
+projection(blank) <- CRS("+proj=longlat +datum=WGS84")
+  
 # Dispersal script
+
 
 disp <- na.omit(read.csv('C:/Users/cjcar/Dropbox/ViralChatter/Data for dispersal.csv'))
 testraster <- raster('D:/ICEBERG/RawENMs/PPM/BinaryLU/Abrocoma_bennettii .tif')
-testname <- "Abrocoma bennettii"
+testname <- "Abrocoma bennettii" # this needs to be part of the parallel
 
 testraster <- raster::resample(testraster,blank,method='ngb')
 dk <- disp[disp$Scientific_name==testname,'disp50']
 buff <- buffer(testraster, dk)
+writeRaster(buff, 'filename') # needs to be fixed
 
