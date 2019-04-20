ca06e7c079dcff8d9489ec4523bd923693d8f706
Greg Albery <gfalbery@gmail.com>
2019-04-20 19:32:41 +0100

    Make run

--- Dispersal maker.R
+++ Dispersal maker.R
@@ -1,3 +1,5 @@
+# Dispersal ####
+
 library(ncdf4)
 library(raster)
 library(parallel)
@@ -5,23 +7,28 @@ library(parallel)
 ### fix blank
 
 blank <- matrix(0,720,360)
-blank <- blanker(blank)
+blank <- raster(blank)
 extent(blank) <- c(-180,180,-90,90)
 projection(blank) <- CRS("+proj=longlat +datum=WGS84")
 
 # Dispersal script
 
-disp <- na.omit(read.csv('C:/Users/cjcar/Dropbox/ViralChatter/Data for dispersal.csv'))
+disp <- Dispersals %>% filter(!is.na(Scientific_name))
 
-i = 1
-j = nrow(disp)
+RasterSp <- list.files("IceMaps") %>% str_split(" ") %>% map(1) %>% sapply(function(a) a[1]) %>% sort
+
+all(RasterSp%in%disp$Scientific_name)
+all(disp$Scientific_name %in% RasterSp)
 
 t1 <- Sys.time()
 
-mclapply(disp$Scientific_name[i:j], function(a){
-  
-  testraster <- raster(paste0('D:/ICEBERG/RawENMs/PPM/BinaryLU/',a,'.tif'))
+i = 1
+j = 2
 
+mclapply(RasterSp[i:j], function(a){
+  
+  testraster <- raster(paste0('IceMaps/',a,' .tif'))
+  
   testraster <- raster::resample(testraster, blank, method = 'ngb')
   dk <- disp[disp$Scientific_name == a, 'disp50']
   buff <- buffer(testraster, dk)
