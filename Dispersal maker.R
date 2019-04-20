f3becd889e3d343a23d9354611f852ca190e3d4e
Greg Albery <gfalbery@gmail.com>
2019-04-20 16:59:02 +0100

    Grebits

--- Dispersal maker.R
+++ Dispersal maker.R
@@ -1,6 +1,6 @@
 library(ncdf4)
 library(raster)
-
+library(parallel)
 
 ### fix blank
 
@@ -8,16 +8,27 @@ blank <- matrix(0,720,360)
 blank <- blanker(blank)
 extent(blank) <- c(-180,180,-90,90)
 projection(blank) <- CRS("+proj=longlat +datum=WGS84")
-  
+
 # Dispersal script
 
-
 disp <- na.omit(read.csv('C:/Users/cjcar/Dropbox/ViralChatter/Data for dispersal.csv'))
-testraster <- raster('D:/ICEBERG/RawENMs/PPM/BinaryLU/Abrocoma_bennettii .tif')
-testname <- "Abrocoma bennettii" # this needs to be part of the parallel
 
-testraster <- raster::resample(testraster,blank,method='ngb')
-dk <- disp[disp$Scientific_name==testname,'disp50']
-buff <- buffer(testraster, dk)
-writeRaster(buff, 'filename') # needs to be fixed
+i = 1
+j = nrow(disp)
 
+t1 <- Sys.time()
+
+mclapply(disp$Scientific_name[i:j], function(a){
+  
+  testraster <- raster(paste0('D:/ICEBERG/RawENMs/PPM/BinaryLU/',a,'.tif'))
+
+  testraster <- raster::resample(testraster, blank, method = 'ngb')
+  dk <- disp[disp$Scientific_name == a, 'disp50']
+  buff <- buffer(testraster, dk)
+  writeRaster(buff, filename = paste0("PostDispersal/",a,'.tif'))
+  
+}, mc.cores = 20)
+
+t2 <- Sys.time()
+
+t2 - t1
