9fe94e678ad7e4417d6f6ec3970b5208f53ffcd3
Greg Albery <gfalbery@gmail.com>
2019-04-22 16:13:53 +0100

    let the right run in

--- Dispersal maker.R
+++ Dispersal maker.R
@@ -17,13 +17,15 @@ Dispersals <- read.csv("data/Data for dispersal.csv", header = T)
 disp <- Dispersals %>% filter(!is.na(Scientific_name), !is.na(disp50))
 disp$Scientific_name <- disp$Scientific_name %>% str_replace(" ", "_")
 
+file.rename(paste0("IceMaps/",list.files("IceMaps")), paste0("IceMaps/",list.files("IceMaps") %>% str_replace(" ","")))
+
 RasterSp <- list.files("IceMaps") %>% 
-  str_split(" ") %>% map(1) %>% 
+  str_split(".tif$") %>% map(1) %>% 
   sapply(function(a) a[1]) %>% sort %>% 
   intersect(disp$Scientific_name)
 
 NoDispSp <- list.files("IceMaps") %>% 
-  str_split(" ") %>% map(1) %>% 
+  str_split(".tif$") %>% map(1) %>% 
   sapply(function(a) a[1]) %>% sort %>% 
   setdiff(disp$Scientific_name)
 
@@ -37,7 +39,7 @@ j = length(RasterSp)
 
 mclapply(RasterSp[i:j], function(a){
   
-  testraster <- raster(paste0('IceMaps/',a,' .tif')) # CAREFUL OF SPACE BEFORE PERIOD
+  testraster <- raster(paste0('IceMaps/',a,'.tif')) # CAREFUL OF SPACE BEFORE PERIOD
   
   testraster <- raster::resample(testraster, blank, method = 'ngb')
   dk <- disp[disp$Scientific_name == a, 'disp50']
@@ -50,22 +52,24 @@ t2 <- Sys.time()
 
 t2 - t1
 
-Success <- list.files("PostDispersal") %>% str_split(".tif") %>% map(1) %>% unlist
+# If not all of them worked, this will rerun them quickly! 
+
+Success <- list.files("PostDispersal") %>% str_split(".tif$") %>% map(1) %>% unlist
 Fail <- setdiff(RasterSp, Success)
 
 mclapply(Fail, function(a){
   
-  testraster <- raster(paste0('IceMaps/',a,' .tif')) # CAREFUL OF SPACE BEFORE PERIOD
+  testraster <- raster(paste0('IceMaps/',a,'.tif')) # CAREFUL OF SPACE BEFORE PERIOD
   
   testraster <- raster::resample(testraster, blank, method = 'ngb')
   dk <- disp[disp$Scientific_name == a, 'disp50']
   buff <- buffer(testraster, dk)
   writeRaster(buff, filename = paste0("PostDispersal/",a,'.tif'))
   
-}, mc.cores = 6)
+}, mc.cores = 15)
 
 a = sample(Success, 1)
-r1 <- raster(paste0('IceMaps/',a,' .tif')) # CAREFUL OF SPACE BEFORE PERIOD
+r1 <- raster(paste0('IceMaps/',a,'.tif')) # CAREFUL OF SPACE BEFORE PERIOD
 r2 <- raster(paste0('PostDispersal/',a,'.tif')) # CAREFUL OF SPACE BEFORE PERIOD
 
 plot(r2)
@@ -75,11 +79,46 @@ plot(r1, add = T, fill = "red")
 # Ironically this doesn't work lol ####
 
 PreDispList <- lapply(list.files("IceMaps")[1:50], function(a) raster(paste0("IceMaps/",a)))
-PostDispList <- lapply(list.files("PostDispersal")[1:50], function(a) raster(paste0("PostDispersal/",a)))
+PostDispList <- lapply(list.files("PostDispersal"), function(a) raster(paste0("PostDispersal/",a)))
 
 PreSizes <- sapply(PreDispList[1:50], function(a) length(na.omit(values(a))))
 PostSizes <- sapply(PostDispList[1:50], function(a) length(na.omit(values(a))))
 
 PreSizes>=PostSizes
+
+# Parsing failures #####
+
+FailList <- lapply(Fail, function(a){
+  
+  raster(paste0('IceMaps/',a,'.tif')) # CAREFUL OF SPACE BEFORE PERIOD
+  
+})
+
+ShouldntFail <- sapply(FailList, function(a){
+  
+  a %>% values %>% na.omit %>% length
+  
+})
+
+
+sapply(Fail[ShouldntFail>0], function(a){
+  
+  testraster <- raster(paste0('IceMaps/',a,'.tif')) # CAREFUL OF SPACE BEFORE PERIOD
+  
+  testraster <- raster::resample(testraster, blank, method = 'ngb')
+  
+  if(testraster %>% values %>% na.omit %>% length == 0) print("AAAAAAAAAAAA") else{
+    
+    dk <- disp[disp$Scientific_name == a, 'disp50']
+    buff <- buffer(testraster, dk)
+    writeRaster(buff, filename = paste0("PostDispersal/",a,'.tif'))
+  }
+})
+
+paste0("IceMaps/",
+       setdiff(list.files("IceMaps"), list.files("PostDispersal")) %>%
+         intersect(paste0(RasterSp, ".tif"))
+) %>% file.remove()
+
 
 
