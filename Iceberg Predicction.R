23c2230cadc2270acb5cce7d059fecb01fd0bd5c
Greg Albery <gfalbery@gmail.com>
2019-04-23 17:31:38 +0100

    Predicc Jagger

--- Iceberg Predicction.R
+++ Iceberg Predicction.R
@@ -6,8 +6,6 @@ PredNetworkList <- list()
 
 load("Output Files/BAMList.Rdata")
 
-tFullSTMatrix <- 1 - (FullSTMatrix - min(FullSTMatrix))/max(FullSTMatrix)
-
 NonEutherians <- c("Diprotodontia",
                    "Dasyuromorphia",
                    "Paucituberculata",
@@ -18,10 +16,15 @@ NonEutherians <- c("Diprotodontia",
                    "Monotremata")
 
 Panth1 <- read.delim("data/PanTHERIA_1-0_WR05_Aug2008.txt") %>%
-  dplyr::rename(Sp = MSW05_Binomial, hOrder = MSW05_Order)
+  dplyr::rename(Sp = MSW05_Binomial, hOrder = MSW05_Order, hFamily = MSW05_Family)
 Panth1$Sp <- Panth1$Sp %>% str_replace(" ", "_")
 
 NonEutherianSp <- Panth1[Panth1$hOrder%in%NonEutherians,"Sp"]
+
+tFullSTMatrix <- 1 - (FullSTMatrix[!rownames(FullSTMatrix)%in%NonEutherianSp,!rownames(FullSTMatrix)%in%NonEutherianSp] - 
+                        min(FullSTMatrix[!rownames(FullSTMatrix)%in%NonEutherianSp,!rownames(FullSTMatrix)%in%NonEutherianSp]))/
+  max(FullSTMatrix[!rownames(FullSTMatrix)%in%NonEutherianSp,!rownames(FullSTMatrix)%in%NonEutherianSp])
+
 
 # IcebergAdjList <- list(CurrentAdj) #, FutureAdj1) #, FutureAdj2, FutureAdj3, FutureAdj4)
 # PredReps <- c("Currents", paste0("Futures", 1:4))
@@ -33,7 +36,7 @@ CORES = 1
 
 for(x in 1:length(IcebergAdjList)){
   
-  FileLoc <- paste0("Iceberg Output Files/", PredReps[1])
+  FileLoc <- paste0("Iceberg Output Files/", PredReps[x])
   
   FullRangeAdj <- IcebergAdjList[[x]]
   
@@ -47,8 +50,8 @@ for(x in 1:length(IcebergAdjList)){
     Space = c(FullRangeAdj[AllMammals,AllMammals]),
     Phylo = c(tFullSTMatrix[AllMammals,AllMammals])
   ) %>% 
-    mutate(Phylo = (Phylo - min(Phylo))/max(Phylo - min(Phylo)),
-           Gz = as.numeric(Space==0)) %>% droplevels
+    mutate(#Phylo = (Phylo - min(Phylo))/max(Phylo - min(Phylo)),
+      Gz = as.numeric(Space==0)) %>% droplevels
   
   UpperMammals <- which(upper.tri(FullSTMatrix[AllMammals, AllMammals], diag = T))
   
@@ -82,60 +85,68 @@ for(x in 1:length(IcebergAdjList)){
   
   print("Predicting All Links!")
   
-  if(file.exists(paste0(FileLoc, "/AllPredList.Rdata"))) load(paste0(FileLoc, "/AllPredList.Rdata")) else{
-    
-    AllIntercept <- attr(AllPredictions1b, "constant")
-    
-    AllPredictions <- AllPredictions1b %>% as.data.frame
-    
-    AllPredictions[,"Intercept"] <- AllIntercept
-    
-    AllPredList <- parallel::mclapply(1:100, function(x){ # to do something non-specific
-      
-      AllPredictions[,"Spp"] <- sample(SpCoef, N, replace = T) + 
-        sample(SpCoef, N, replace = T)
-      
-      BinPred <- rbinom(n = N,
-                        prob = logistic(rowSums(AllPredictions)),
-                        size  = 1)
-      
-      BinPred
-      
-    }, mc.cores = CORES)
-    
-    save(AllPredList, file = paste0(FileLoc, "/AllPredList.Rdata"))
-    
-  }
   
-  print("Simulating All Networks!")
+  AllIntercept <- attr(AllPredictions1b, "constant")
   
-  # Simulating the network #####
+  AllPredictions <- AllPredictions1b %>% as.data.frame
   
-  if(file.exists(paste0(FileLoc, "/AllSims.Rdata"))) load(paste0(FileLoc,"/AllSims.Rdata")) else{
+  AllPredictions[,"Intercept"] <- AllIntercept
+  
+  DoNetworks = F
+  
+  if(DoNetworks){
     
-    AllSims <- parallel::mclapply(1:length(AllPredList), function(i){
+    if(file.exists(paste0(FileLoc, "/AllPredList.Rdata"))&DoNetworks) load(paste0(FileLoc, "/AllPredList.Rdata")) else{
       
-      AssMat <- matrix(NA, 
-                       nrow = length(AllMammals), 
-                       ncol = length(AllMammals))
       
-      AssMat[lower.tri(AssMat)] <- round(AllPredList[[i]])
-      AssMat[upper.tri(AssMat)] <- t(AssMat)[!is.na(t(AssMat))]
-      diag(AssMat) <- 0
-      dimnames(AssMat) <- list(union(AllMammaldf$Sp,AllMammaldf$Sp2),
-                               union(AllMammaldf$Sp,AllMammaldf$Sp2))
+      AllPredList <- parallel::mclapply(1:100, function(x){ # to do something non-specific
+        
+        AllPredictions[,"Spp"] <- sample(SpCoef, N, replace = T) + 
+          sample(SpCoef, N, replace = T)
+        
+        BinPred <- rbinom(n = N,
+                          prob = logistic(rowSums(AllPredictions)),
+                          size  = 1)
+        
+        BinPred
+        
+      }, mc.cores = CORES)
       
-      as(AssMat, "dgCMatrix")
-      
-    }, mc.cores = CORES)
+      save(AllPredList, file = paste0(FileLoc, "/AllPredList.Rdata"))
+    } 
     
-    if(length(which(sapply(AllSims, is.null)))>0){
-      AllSims <- AllSims[-which(sapply(AllSims, is.null))]
-      print("Something went wrong UGH")
-      print(paste("New Length = ", length(AllSims)))
+    
+    print("Simulating All Networks!")
+    
+    # Simulating the network #####
+    
+    if(file.exists(paste0(FileLoc, "/AllSims.Rdata"))) load(paste0(FileLoc,"/AllSims.Rdata")) else{
+      
+      AllSims <- parallel::mclapply(1:length(AllPredList), function(i){
+        
+        AssMat <- matrix(NA, 
+                         nrow = length(AllMammals), 
+                         ncol = length(AllMammals))
+        
+        AssMat[lower.tri(AssMat)] <- round(AllPredList[[i]])
+        AssMat[upper.tri(AssMat)] <- t(AssMat)[!is.na(t(AssMat))]
+        diag(AssMat) <- 0
+        dimnames(AssMat) <- list(union(AllMammaldf$Sp,AllMammaldf$Sp2),
+                                 union(AllMammaldf$Sp,AllMammaldf$Sp2))
+        
+        as(AssMat, "dgCMatrix")
+        
+      }, mc.cores = CORES)
+      
+      if(length(which(sapply(AllSims, is.null)))>0){
+        AllSims <- AllSims[-which(sapply(AllSims, is.null))]
+        print("Something went wrong UGH")
+        print(paste("New Length = ", length(AllSims)))
+      }
+      
+      save(AllSims, file = paste0(FileLoc, "/AllSims.Rdata"))
+      
     }
-    
-    save(AllSims, file = paste0(FileLoc, "/AllSims.Rdata"))
     
   }
   
@@ -144,8 +155,6 @@ for(x in 1:length(IcebergAdjList)){
   print("Summing Matrix!")
   
   if(file.exists(paste0(FileLoc,"/AllSums.Rdata"))) load(paste0(FileLoc,"/AllSums.Rdata")) else{
-    
-    AllPredDF <- AllPredList %>% as.data.frame()
     
     AllPredSums <- logistic(rowSums(AllPredictions))
     
@@ -170,17 +179,15 @@ for(x in 1:length(IcebergAdjList)){
   
 }
 
+save(PredNetworkList, file = "Iceberg Output Files/PredNetworkList.Rdata")
+
 MatrixPoints(PredNetworkList[[1]], 
              PredNetworkList[[2]], 
              Names = AllMammals[1:1000], 
              Axes = c("Current", "Future")) + 
   coord_fixed()
 
-
-DeltaSharingList <- list()
-
 # Comparing currents and futures ####
-
 
 AllMammals2 <- reduce(list(rownames(FullSTMatrix), 
                            rownames(IcebergAdjList[[1]]), 
@@ -196,19 +203,43 @@ AllMammalMatrix <- data.frame(
   SpaceFuture = c(IcebergAdjList[[2]][AllMammals2,AllMammals2]),
   Phylo = c(tFullSTMatrix[AllMammals2,AllMammals2])
 ) %>% 
-  mutate(Phylo = (Phylo - min(Phylo))/max(Phylo - min(Phylo)),
-         Gz = as.numeric(SpaceCurrent==0&SpaceFuture==0)) %>% droplevels
+  mutate(#Phylo = (Phylo - min(Phylo))/max(Phylo - min(Phylo)),
+    Gz = as.numeric(SpaceCurrent==0&SpaceFuture==0),
+    DeltaOverlap = SpaceFuture - SpaceCurrent,
+    NewOverlap = as.numeric(SpaceCurrent == 0 & SpaceFuture>0)) %>% droplevels
 
 UpperMammals <- which(upper.tri(FullSTMatrix[AllMammals2, AllMammals2], diag = T))
 
 AllMammaldf2 <- AllMammalMatrix[-UpperMammals,]
 
-DeltaSharing <- PredNetworkList[[2]][AllMammals2, AllMammals2] - PredNetworkList[[1]][AllMammals2, AllMammals2]
+AllMammaldf2 <- AllMammaldf2 %>% #filter(SpaceCurrent == 0 & SpaceFuture>0) %>% 
+  mutate(CurrentSharing = c(as.matrix(PredNetworkList[[1]])[AllMammals2, AllMammals2])[-UpperMammals],
+         FutureSharing = c(as.matrix(PredNetworkList[[2]])[AllMammals2, AllMammals2])[-UpperMammals],
+         DeltaSharing = FutureSharing - CurrentSharing) %>%
+  slice(order(DeltaSharing, decreasing = T)) %>%
+  inner_join(Panth1[,c("Sp", "hFamily", "hOrder")], by = c("Sp" = "Sp")) %>%
+  inner_join(Panth1[,c("Sp", "hFamily", "hOrder")], by = c("Sp2" = "Sp"))
 
-AllMammaldf2$DeltaSharing <- c(DeltaSharing[-UpperMammals,])
+NewEncounters <- AllMammaldf2 %>% filter(SpaceCurrent == 0 & SpaceFuture>0)
 
-AllMammaldf2 %>% filter(Gz==0) %>% ggplot(aes(DeltaSharing)) + geom_density()
+NewEncounters %>% group_by(hOrder.x, hOrder.y) %>% 
+  summarise(DeltaSharing = mean(DeltaSharing), N = n()) %>% 
+  ggplot(aes(hOrder.x, hOrder.y, fill = DeltaSharing)) + 
+  geom_tile() + 
+  coord_fixed() + 
+  geom_text(aes(label = N)) + 
+  scale_fill_continuous_sequential(palette = "reds") 
 
-AllMammaldf2 %>% filter(SpaceCurrent==0&SpaceFuture==1) %>% select(Sp, Sp)
+AllMammaldf2 %>% slice(order(DeltaSharing, decreasing = T)) %>% head(25)
 
+# Making the raster overlaps ####
 
+names(RasterBrick) <- names(RasterBrick) %>% str_replace("[.]", "_")
+
+NewOverlapSp <- unique(c(NewEncounters$Sp, NewEncounters$Sp2))
+
+BinaryNewOverlaps <- as.matrix(IcebergAdjList[[1]])[AllMammals2,AllMammals2]==0&as.matrix(IcebergAdjList[[2]])[AllMammals2,AllMammals2]>0
+
+NewIntersects <- IntersectGet(RasterBrick, 
+                              Names = NewOverlapSp, 
+                              Predicate = BinaryNewOverlaps)
