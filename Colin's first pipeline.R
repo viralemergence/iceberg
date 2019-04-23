dfa202c93d31798ac1c7004f92c0c4cd4f2dd017
CJ Carlson <cjcarlson@berkeley.edu>
2019-04-22 23:04:57 -0400

    Stark>Cap you little baby

--- Colin's first pipeline.R
+++ Colin's first pipeline.R
@@ -27,7 +27,33 @@ RasterListb <- lapply(1:length(RasterLista), function(a){
       print(a)
       raster::resample(RasterLista[[a]], blank, method = 'ngb')
 })
+
+
+RasterListb <- lapply(1:length(RasterLista), function(a){
+  print(a)
+  raster::resample(RasterLista[[a]], blank, method = 'ngb')
+})
+
     
 RasterBrick <- raster::brick(RasterListb)
 names(RasterBrick) <- shortnames %>% str_remove(".tif$")
-    
+s <- sum(RasterBrick,na.rm = FALSE)
+
+library(rasterVis); library(maps)
+colors <- colorRampPalette(brewer.pal(10,"YlGn"))
+levelplot(s, col.regions=colors)
+map('world',add=TRUE)
+
+
+########## 
+
+library(ncdf4); setwd("D:/ICEBERG")
+ncin <- nc_open("states.nc")
+get <- function(varname) {
+  soilm_array <- ncvar_get(ncin,varname,start=c(1,1,1166))
+  slice <- raster(t(soilm_array))
+  raster::extent(slice) <- c(-180,180,-90,90)
+  return(slice)
+}
+lutypes <- names(ncin$var)[1]
+landuse2017 <- stack(lapply(lutypes,get))
