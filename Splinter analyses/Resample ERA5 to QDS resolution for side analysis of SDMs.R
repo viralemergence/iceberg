5ddf5c5020f97116df40606c1fefda1cc3b2d5f3
Colin J. Carlson <cjcarlson@berkeley.edu>
2020-12-06 16:12:57 -0500

    annotation so i don't forget,

--- Splinter analyses/Resample ERA5 to QDS resolution for side analysis of SDMs.R
+++ Splinter analyses/Resample ERA5 to QDS resolution for side analysis of SDMs.R
@@ -4,6 +4,9 @@ library(ncdf4)
 tas <- brick("C:/Users/cjcar/Downloads/adaptor.mars.internal-1605386567.6335182-13770-31-377b4dc6-4585-4c7e-88fb-57b7de0a8859.nc")
 pre <- brick("C:/Users/cjcar/Downloads/adaptor.mars.internal-1605386711.0461376-16515-23-dfb714e2-04ed-42b3-b4c5-805def808674.nc")
 
+tas <- rotate(tas)
+pre <- rotate(pre)
+
 blank <- matrix(0,360*2,720*2) # proper resolution
 blank <- raster(blank)
 extent(blank) <- c(-180, 180, -90, 90)
@@ -12,7 +15,7 @@ projection(blank) <- CRS("+proj=longlat +datum=WGS84")
 pre <- resample(pre, blank)
 tas <- resample(tas, blank)
 
-tas <- tas[[1:360]]
+tas <- tas[[1:360]] # Because I accidentally included 2020
 writeRaster(pre, "C:/Users/cjcar/Dropbox/ERA5 Iceberg/pre.nc")
 writeRaster(tas, "C:/Users/cjcar/Dropbox/ERA5 Iceberg/tas.nc")
 
